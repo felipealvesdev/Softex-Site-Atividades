7cabf4af2d3846d1bdd26ff744fc44fa9e5222f8
Felipe Alves <felipealexandre1408@gmail.com>
2023-07-28 09:50:32 -0300

    Removendo atribuicoes desnecessarias parte2

--- calculadoraEmLoop.js
+++ calculadoraEmLoop.js
@@ -1,23 +1,24 @@
-const numero1 = parseFloat(prompt("Digite o primeiro número:"));
-const numero2 = parseFloat(prompt("Digite o segundo número:"));
 const operacao = parseInt(prompt("Digite a opção de operador desejado: 1.Soma 2.Subtração 3.Multiplicação 4.Divisão"));
-let result;
 
-function calculadora(num1, num2, operac){
+
+function calculadora(operac){
+    const numero1 = parseFloat(prompt("Digite o primeiro número:"));
+    const numero2 = parseFloat(prompt("Digite o segundo número:"));
+    let result;
 
     if(operac === 1){
         result = num1 + num2;
         alert(`Resultado de ${num1} + ${num2} = ${result}`);
-    }else if(operac === 2){
+    }else if(operacao === 2){
         result = num1 - num2;
         alert(`Resultado de ${num1} - ${num2} = ${result}`);
-    }else if(operac === 3){
+    }else if(operacao === 3){
         result = num1 * num2;
         alert(`Resultado de ${num1} * ${num2} = ${result}`);
-    }else if(operac === 4){
+    }else if(operacao === 4){
         result = num1 / num2;
         alert(`Resultado de ${num1} / ${num2} = ${result}`);
-    }else{
+    }else if(operac){
         result = 0;
         alert(result);
         alert("Por favor, insira um número entre 1 e 4 para realizar uma operação.")
