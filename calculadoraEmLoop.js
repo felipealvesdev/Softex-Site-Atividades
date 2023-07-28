76a144b2be7f59d8f343c05dbd446533fba35039
Felipe Alves <felipealexandre1408@gmail.com>
2023-07-28 10:04:00 -0300

    Adicionando a calculadora em loop com funcao de parar programa

--- calculadoraEmLoop.js
+++ calculadoraEmLoop.js
@@ -1,5 +1,4 @@
-const operacao = parseInt(prompt("Digite a opção de operador desejado: 1.Soma 2.Subtração 3.Multiplicação 4.Divisão"));
-
+let ligado = true;
 
 function calculadora(operac){
     const numero1 = parseFloat(prompt("Digite o primeiro número:"));
@@ -7,21 +6,29 @@ function calculadora(operac){
     let result;
 
     if(operac === 1){
-        result = num1 + num2;
-        alert(`Resultado de ${num1} + ${num2} = ${result}`);
-    }else if(operacao === 2){
-        result = num1 - num2;
-        alert(`Resultado de ${num1} - ${num2} = ${result}`);
-    }else if(operacao === 3){
-        result = num1 * num2;
-        alert(`Resultado de ${num1} * ${num2} = ${result}`);
-    }else if(operacao === 4){
-        result = num1 / num2;
-        alert(`Resultado de ${num1} / ${num2} = ${result}`);
-    }else if(operac){
+        result = numero1 + numero2;
+        alert(`Resultado de ${numero1} + ${numero2} = ${result}`);
+    }else if(operac === 2){
+        result = numero1 - numero2;
+        alert(`Resultado de ${numero1} - ${numero2} = ${result}`);
+    }else if(operac === 3){
+        result = numero1 * numero2;
+        alert(`Resultado de ${numero1} * ${numero2} = ${result}`);
+    }else if(operac === 4){
+        result = numero1 / numero2;
+        alert(`Resultado de ${numero1} / ${numero2} = ${result}`);
+    }else{
         result = 0;
         alert(result);
-        alert("Por favor, insira um número entre 1 e 4 para realizar uma operação.")
+        alert("Por favor, insira um número entre 1 e 5 para realizar uma operação.");
     }
 }
-calculadora(numero1, numero2, operacao);
+while(ligado === true){
+    const operacao = parseInt(prompt("Digite a opção de operador desejado: 1.Soma 2.Subtração 3.Multiplicação 4.Divisão 0.Parar programa"));
+    if(operacao !== 0){
+        calculadora(operacao);
+    }else{
+        ligado = false;
+        alert("Programa será fechado.");
+    }
+}
