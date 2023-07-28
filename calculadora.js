df2f7fca495bf5c0cf0879cb6bd38672fd958479
Felipe Alves <felipealexandre1408@gmail.com>
2023-07-28 09:40:34 -0300

    Modificando o programa para usar calculadora como funcao

--- calculadora.js
+++ calculadora.js
@@ -3,20 +3,27 @@ const numero2 = parseFloat(prompt("Digite o segundo número:"));
 const operacao = parseInt(prompt("Digite a opção de operador desejado: 1.Soma 2.Subtração 3.Multiplicação 4.Divisão"));
 let result;
 
-if(operacao === 1){
-    result = numero1 + numero2;
-    alert(`Resultado de ${numero1} + ${numero2} = ${result}`);
-}else if(operacao === 2){
-    result = numero1 - numero2;
-    alert(`Resultado de ${numero1} - ${numero2} = ${result}`);
-}else if(operacao === 3){
-    result = numero1 * numero2;
-    alert(`Resultado de ${numero1} * ${numero2} = ${result}`);
-}else if(operacao === 4){
-    result = numero1 / numero2;
-    alert(`Resultado de ${numero1} / ${numero2} = ${result}`);
-}else{
-    result = 0;
-    alert(result);
-    alert("Por favor, insira um número entre 1 e 4 para realizar uma operação.")
+function calculadora(num1, num2, operac){
+    num1 = numero1;
+    num2 = numero2;
+    operac = operacao;
+
+    if(operac === 1){
+        result = num1 + num2;
+        alert(`Resultado de ${num1} + ${num2} = ${result}`);
+    }else if(operacao === 2){
+        result = num1 - num2;
+        alert(`Resultado de ${num1} - ${num2} = ${result}`);
+    }else if(operacao === 3){
+        result = num1 * num2;
+        alert(`Resultado de ${num1} * ${num2} = ${result}`);
+    }else if(operacao === 4){
+        result = num1 / num2;
+        alert(`Resultado de ${num1} / ${num2} = ${result}`);
+    }else{
+        result = 0;
+        alert(result);
+        alert("Por favor, insira um número entre 1 e 4 para realizar uma operação.")
+    }
 }
+calculadora(numero1, numero2, operacao);
